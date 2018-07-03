da7ed4d64146859e0f49952167c80ac531c61d9c
Zhong Huiwen <huiwen@aporigin.com>
2018-07-03 17:23:55 +0800

    Allow horizontal scrolling from https://github.com/santi6291/iNoBounce

--- inobounce.js
+++ inobounce.js
@@ -1,9 +1,11 @@
+/* eslint-disable */ // REMOVEEEEE
 /*! iNoBounce - v0.1.6
 * https://github.com/lazd/iNoBounce/
 * Copyright (c) 2013 Larry Davis <lazdnet@gmail.com>; Licensed BSD */
 (function(global) {
 	// Stores the Y position where the touch started
-	var startY = 0;
+  var startY = 0;
+  var startX = 0;
 
 	// Store enabled status
 	var enabled = false;
@@ -44,25 +46,20 @@
 			}
 
 			var scrolling = style.getPropertyValue('-webkit-overflow-scrolling');
-			var overflowY = style.getPropertyValue('overflow-y');
+      var overflowY = style.getPropertyValue('overflow-y');
+      var overflowX = style.getPropertyValue('overflow-x');
 			var height = parseInt(style.getPropertyValue('height'), 10);
 
 			// Determine if the element should scroll
-			var isScrollable = scrolling === 'touch' && (overflowY === 'auto' || overflowY === 'scroll');
-			var canScroll = el.scrollHeight > el.offsetHeight;
+			var isScrollable = isScrollableCheck(scrolling, overflowY, overflowX);
+			var canScroll = canScrollCheck(overflowY, el);
 
 			if (isScrollable && canScroll) {
-				// Get the current Y position of the touch
-				var curY = evt.touches ? evt.touches[0].screenY : evt.screenY;
 
-				// Determine if the user is trying to scroll past the top or bottom
-				// In this case, the window will bounce, so we have to prevent scrolling completely
-				var isAtTop = (startY <= curY && el.scrollTop === 0);
-				var isAtBottom = (startY >= curY && el.scrollHeight - el.scrollTop === height);
-
-				// Stop a bounce bug when at the bottom or top of the scrollable element
-				if (isAtTop || isAtBottom) {
-					evt.preventDefault();
+				if (overflowY === 'auto' || overflowY === 'scroll') {
+					horScroll(evt, height, el)
+				} else {
+					vertScroll(evt)
 				}
 
 				// No need to continue up the DOM, we've done our job
@@ -77,12 +74,62 @@
 		evt.preventDefault();
 	};
 
+	// ensure user is scrolling horizontally
+	var vertScroll = function(evt){
+		// Get the current Y position of the touch
+		var curY = evt.touches ? evt.touches[0].screenY : evt.screenY;
+		// Get the current X position of the touch
+		var curX = evt.touches ? evt.touches[0].screenX : evt.screenX;
+
+		var Ydiff = Math.abs(startY-curY)
+		var Xdiff = Math.abs(startX-curX)
+
+		// prevent if the user tried to scroll vertical in horizontal area
+		if (Ydiff > Xdiff) {
+			evt.preventDefault();
+		}
+	}
+
+  var horScroll = function(evt, height, el){
+    // Get the current Y position of the touch
+    var curY = evt.touches ? evt.touches[0].screenY : evt.screenY;
+
+    // Determine if the user is trying to scroll past the top or bottom
+    // In this case, the window will bounce, so we have to prevent scrolling completely
+    var isAtTop = (startY <= curY && el.scrollTop === 0);
+    var isAtBottom = (startY >= curY && el.scrollHeight - el.scrollTop === height);
+
+    // Stop a bounce bug when at the bottom or top of the scrollable element
+    // Only need this for vertical scrolling
+    if ( isAtTop || isAtBottom) {
+      evt.preventDefault();
+    }
+  }
+
+	var canScrollCheck = function(overflowY, el){
+		if (overflowY === 'auto' || overflowY === 'scroll') {
+			return el.scrollHeight > el.offsetHeight
+		}
+
+		return el.scrollWidth > el.offsetWidth;
+	}
+
+  var isScrollableCheck = function(scrolling, overflowY, overflowX){
+		isTouchScroll = scrolling === 'touch';
+		scrollY = (overflowY === 'auto' || overflowY === 'scroll');
+		scrollX = (overflowX === 'auto' || overflowX === 'scroll');
+
+		return isTouchScroll && (scrollY || scrollX);
+  }
+
 	var handleTouchstart = function(evt) {
 		// Store the first Y position of the touch
-		startY = evt.touches ? evt.touches[0].screenY : evt.screenY;
+    startY = evt.touches ? evt.touches[0].screenY : evt.screenY;
+		// Store the first X position of the touch
+		startX = evt.touches ? evt.touches[0].screenX : evt.screenX;
 	};
 
-	var enable = function() {
+  var enable = function() {
 		// Listen to a couple key touch events
 		window.addEventListener('touchstart', handleTouchstart, supportsPassiveOption ? { passive : false } : false);
 		window.addEventListener('touchmove', handleTouchmove, supportsPassiveOption ? { passive : false } : false);
